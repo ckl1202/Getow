4320eeca84acee054de0e70a372d7a2e1cb9f7c8
ckl1202@sjtu.edu.cn <ckl1202@sjtu.edu.cn>
2016-05-22 17:39:32 +0800

    implement the body of read and modify ins list.

--- gui/changeInsUI.py
+++ gui/changeInsUI.py
@@ -8,6 +8,7 @@ from changeIns import *
 import tkMessageBox
 
 def GetInsText(insText):
+#获取当前UI中的合约信息
 	insList = insText.get(1.0, END)
 	insList = insList.split('\n')
 	for i in range(len(insList) - 1, -1, -1):
@@ -17,11 +18,13 @@ def GetInsText(insText):
 	
 
 def SaveInsText(insText):
+#保存合约信息
 	insList = GetInsText(insText)
-	print insList
+	SaveNowIns(insList)
 	tkMessageBox.showinfo(message = "保存成功")
 
 def changeInsList():
+#主界面
 	root = Tk()
 	insText = Text(root, width = 30, height = 15)
 	insText.grid(columnspan = 2)
--- kernel/changeIns.py
+++ kernel/changeIns.py
@@ -2,6 +2,23 @@
 # -*- coding:utf-8 -*-
 
 def GetNowInsList():
-	return ['IF1609', 'IC1609', 'IH1609']
+	insFile = open("../dataGetter/insList.in", "r")
+	a = int(insFile.readline())
+	insList = []
+	for i in range(0, a):
+		str = insFile.readline()
+		str = str[:-1]
+		insList.append(str)
+	insFile.close()
+	return insList
+
+def SaveNowIns(insList):
+	insFile = open("../dataGetter/insList.in", "w")
+	insFile.write(str(len(insList)))
+	insFile.write('\n')
+	for item in insList:
+		insFile.write(item)
+		insFile.write('\n')
+	insFile.close()
 
 
